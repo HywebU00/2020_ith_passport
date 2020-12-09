3d7d44b6cf7da6d69be360326b5574c35ce12d72
anniewu <annie@hyweb.com.tw>
2020-12-09 16:17:09 +0800

    20201209 新增燈箱及修改js

--- js/hyui.js
+++ js/hyui.js
@@ -663,20 +663,20 @@ $(function() {
             $('.font_size').find('.small').parent('li').siblings('li').find('a').removeClass('active');
             $('body').removeClass('large_size medium_size').addClass('small_size');
             $('.font_size').find('.small').addClass('active');
-            e.preventDefault();
+            // e.preventDefault();
         } else {
             if (cookie == 'large') {
                 //$('.font_size').find('.large').click();
                 $('.font_size').find('.large').parent('li').siblings('li').find('a').removeClass('active');
                 $('body').removeClass('small_size medium_size').addClass('large_size');
                 $('.font_size').find('.large').addClass('active');
-                e.preventDefault();
+                // e.preventDefault();
             } else {
                 //$('.font_size').find('.medium').click();
                 $('.font_size').find('.medium').parent('li').siblings('li').find('a').removeClass('active');
                 $('body').removeClass('large_size small_size');
                 $('.font_size').find('.medium').addClass('active');
-                e.preventDefault();
+                // e.preventDefault();
             }
         }
     });
